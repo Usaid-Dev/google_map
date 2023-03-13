7e3a1180b50054d57896d2e9cd48c05da0765406
Usaid Asif <usaidasif12@gmail.com>
2023-03-13 13:16:31 +0500

    Convert coordinates (lat/lang) to address

---
+++ lib/lang_to_address.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+
+class ConvertLatLangToAddress extends StatefulWidget {
+  const ConvertLatLangToAddress({super.key});
+
+  @override
+  State<ConvertLatLangToAddress> createState() =>
+      _ConvertLatLangToAddressState();
+}
+
+class _ConvertLatLangToAddressState extends State<ConvertLatLangToAddress> {
+  String stAddress = '', stAdd = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Map'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(stAddress),
+          Text(stAdd),
+          GestureDetector(
+            onTap: () async {
+              List<Location> locations =
+                  await locationFromAddress("Gronausestraat 710, Enschede");
+
+              List<Placemark> placemarks =
+                  await placemarkFromCoordinates(52.2165157, 6.9437819);
+
+              setState(
+                () {
+                  stAddress = locations.last.latitude.toString() +
+                      " " +
+                      locations.last.longitude.toString();
+
+                  stAdd = placemarks.reversed.last.country.toString() +
+                      " " +
+                      placemarks.reversed.last.locality.toString();
+                },
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                decoration: const BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: const Center(
+                  child: Text('Convert'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'home_screen.dart';
+import 'package:google_map/lang_to_address.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const ConvertLatLangToAddress(),
     );
   }
 }
