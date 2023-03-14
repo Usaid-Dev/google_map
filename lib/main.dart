275ac9f7d149cedeedb8f46cf107115f968726e8
Usaid Asif <usaidasif12@gmail.com>
2023-03-14 14:08:50 +0500

    Google map complete places api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_map/user_current_location.dart';
+import 'google_places_api.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const GetUserCurrentLocation(),
+      home: const GooglePlacesApiScreen(),
     );
   }
 }
