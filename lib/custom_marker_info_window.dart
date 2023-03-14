f2e7703756e63e70141924909e1b767206483cc9
Usaid Asif <usaidasif12@gmail.com>
2023-03-14 20:58:47 +0500

    Custom info window

---
+++ lib/custom_marker_info_window.dart
@@ -0,0 +1,182 @@
+import 'package:custom_info_window/custom_info_window.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomMarkerInfoWindow extends StatefulWidget {
+  const CustomMarkerInfoWindow({super.key});
+
+  @override
+  State<CustomMarkerInfoWindow> createState() => _CustomMarkerInfoWindowState();
+}
+
+class _CustomMarkerInfoWindowState extends State<CustomMarkerInfoWindow> {
+  CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
+
+  final List<Marker> _markers = <Marker>[];
+
+  final List<LatLng> _latLang = <LatLng>[
+    const LatLng(24.8825, 67.0694),
+    const LatLng(24.871641, 67.059906),
+    const LatLng(24.8937, 67.2163),
+    const LatLng(24.8784, 67.0103),
+    const LatLng(24.9047, 67.0472),
+    const LatLng(24.8779, 67.0874)
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() {
+    for (int i = 0; i < _latLang.length; i++) {
+      if (i % 2 == 0) {
+        _markers.add(
+          Marker(
+            markerId: MarkerId(i.toString()),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _latLang[i],
+            onTap: () {
+              _customInfoWindowController.addInfoWindow!(
+                Container(
+                  height: 300,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                ),
+                _latLang[i],
+              );
+            },
+          ),
+        );
+      } else {
+        _markers.add(
+          Marker(
+            markerId: MarkerId(i.toString()),
+            icon: BitmapDescriptor.defaultMarker,
+            position: _latLang[i],
+            onTap: () {
+              _customInfoWindowController.addInfoWindow!(
+                Container(
+                  height: 300,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?cs=srgb&dl=pexels-narda-yescas-1566837.jpg&fm=jpg',
+                            ),
+                            fit: BoxFit.fitWidth,
+                            filterQuality: FilterQuality.high,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: Colors.red,
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 10, left: 10, right: 10),
+                        child: Row(
+                          children: const [
+                            SizedBox(
+                              width: 100,
+                              child: Text(
+                                'Beef Tacos',
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              '.3 mi.',
+                              // widget.data!.date!,
+                            )
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                        child: Text(
+                          'Help me finish these tacos! I got a platter from Costco and it’s too much.',
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _latLang[i],
+              );
+            },
+          ),
+        );
+      }
+
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Custom Info Window Example"),
+        backgroundColor: Colors.red,
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(24.8825, 67.0694),
+              zoom: 14,
+            ),
+            markers: Set.of(_markers),
+            onTap: (position) {
+              _customInfoWindowController.hideInfoWindow!();
+            },
+            onCameraMove: (position) {
+              _customInfoWindowController.onCameraMove!();
+            },
+            onMapCreated: (GoogleMapController controller) {
+              _customInfoWindowController.googleMapController = controller;
+            },
+          ),
+          CustomInfoWindow(
+            controller: _customInfoWindowController,
+            height: 200,
+            width: 300,
+            offset: 35,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'custom_marker_screen.dart';
+import 'custom_marker_info_window.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CustomMarkerScreen(),
+      home: const CustomMarkerInfoWindow(),
     );
   }
 }
