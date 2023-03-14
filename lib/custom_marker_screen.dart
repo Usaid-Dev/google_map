2db1da5804cec2d2ab67d812b5d3bdd2f04efc7a
Usaid Asif <usaidasif12@gmail.com>
2023-03-14 15:56:48 +0500

    Add multiple custom marker on map

---
+++ lib/custom_marker_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class CustomMarkerScreen extends StatefulWidget {
+  const CustomMarkerScreen({Key? key}) : super(key: key);
+
+  @override
+  _CustomMarkerScreenState createState() => _CustomMarkerScreenState();
+}
+
+class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  List<String> images = [
+    'images/car.png',
+    'images/car2.png',
+    'images/marker2.png',
+    'images/marker3.png',
+    'images/marker.png',
+    'images/motorcycle.png',
+  ];
+
+  Uint8List? markerImage;
+  final List<Marker> _markers = <Marker>[];
+  final List<LatLng> _latLang = <LatLng>[
+    const LatLng(24.8825, 67.0694),
+    const LatLng(24.871641, 67.059906),
+    const LatLng(24.8937, 67.2163),
+    const LatLng(24.8784, 67.0103),
+    const LatLng(24.9047, 67.0472),
+    const LatLng(24.8779, 67.0874)
+  ];
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(24.8607, 67.0011),
+    zoom: 15,
+  );
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    for (int i = 0; i < images.length; i++) {
+      final Uint8List markerIcon =
+          await getBytesFromAsset(images[i].toString(), 100);
+      _markers.add(Marker(
+          markerId: MarkerId(i.toString()),
+          position: _latLang[i],
+          icon: BitmapDescriptor.fromBytes(markerIcon),
+          infoWindow: const InfoWindow(title: 'The title of the marker')));
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Customer Marker'),
+      ),
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          mapType: MapType.normal,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          markers: Set<Marker>.of(_markers),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'google_places_api.dart';
+import 'custom_marker_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const GooglePlacesApiScreen(),
+      home: const CustomMarkerScreen(),
     );
   }
 }
